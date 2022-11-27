440e63c60a4dfd4b19fece888a33732de38a9b2f
manos <jaiswartony@gmail.com>
2022-11-27 19:07:17 +0530

    First Commit Raw Started

---
+++ Complain/src/test/java/com/abctelecom/complain/ComplainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abctelecom.complain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
